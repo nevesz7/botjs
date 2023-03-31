0d535966706e7feb47921e2b660d32064dd5a44c
Nevesz7 <raphael.nevesz@live.com>
2023-03-31 12:55:06 -0300

    Finished server.js and tested on browser

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { graphql, buildSchema } = require('graphql');
+
+//Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+	type Query {
+		hello : String
+	}
+`);
+
+// The rootValue provides a resolver function for each API endpoint
+var root = {
+	hello: () => {
+		return 'Hello Taqos!';
+	}
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+	schema: schema,
+	rootValue: root,
+	graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
