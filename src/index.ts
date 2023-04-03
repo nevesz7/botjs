011504cc9513d1d9f4512b61bce1917564309beb
Nevesz7 <raphael.nevesz@live.com>
2023-04-03 13:23:00 -0300

    apollo server with books query

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = `
+	type Book {
+		title: String
+		author: String
+	}
+	type Query {
+		books: [Book]
+	}
+`;
+
+const books = [
+	{
+		title: 'Eragon',
+		author: 'Chistopher Paolini',
+	},
+	{
+		title: 'Golden Compass',
+		author: 'Philip Pullman'
+	},
+	{
+		title: 'Maze Runner',
+		author: 'James Dashner',
+	},
+];
+
+const resolvers = {
+	Query: {
+		books: () => books,
+	},
+};
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+const start=async() => {
+	const { url } = await startStandaloneServer (server, { listen: { port: 4000 } });
+	console.log("Server ready at http://localhost:4000/");
+};
+
+start();
