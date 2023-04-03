df0924bc0b9745cc2e33fa0e23ea7e55365a4827
Nevesz7 <raphael.nevesz@live.com>
2023-04-03 18:14:20 -0300

    eslint pointing errors on real time

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,13 @@
 module.exports = {
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint', "prettier"],
-	rules: {
-		"prettier/prettier": "error"
-	},
-	env: {
-		browser: true,
-		node: true,
-	},
-	root: true,
-  };
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  root: true,
+};
