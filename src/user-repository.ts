db9c76dcd12dd65e009dd469405411c55a8c0ee9
Nevesz7 <raphael.nevesz@live.com>
2023-04-06 03:14:19 -0300

    sleeping

---
+++ src/user-repository.ts
@@ -0,0 +1,12 @@
+import { EntityRepository, Repository } from "typeorm";
+import { User } from "./entities/user";
+import { userInfo } from "os";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+	findByName(name: string) {
+		return this.createQueryBuilder("user")
+		.where("user.name = :name", { name })
+		.getOne();
+	}
+}
