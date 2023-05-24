6787082feb99b9b429351514981b2a6310e4bc0a
Nevesz7 <raphael.nevesz@live.com>
2023-05-24 15:09:14 -0300

    secret from env

--- src/token.ts
+++ src/token.ts
@@ -1,7 +1,8 @@
+// import * as dotenv from "dotenv";
 import { sign } from "jsonwebtoken";
 
 export const getToken = (user, rememberMe: boolean) => {
-  const secret = "nelson";
+  const secret = process.env.secret;
   let token: string;
   if (rememberMe === false) {
     token = sign(user, secret, { expiresIn: 3600 });
